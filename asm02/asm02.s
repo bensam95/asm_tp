0a5fd4cdd185ddedcc24a836d0176f2e61f75e39
bensam95 <bensam.dev@gmail.com>
2026-09-07 22:47:26 +0200

    upd

---
+++ asm02/asm02.s
@@ -0,0 +1,16 @@
+global_start
+
+section .data
+msg db "1337" , 10
+len equ $ - msg
+section .text
+
+    mov rax, 1
+    mov rdi, 1 
+    mov rsi, msg
+    mov rdx, len 
+    syscall
+
+    mov rax, 60
+    mov rdi, 0 
+    syscall

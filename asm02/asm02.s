7f533188ce5f22e7702f84463b7515fe0a7846b4
bensam95 <bensam.dev@gmail.com>
2026-09-08 19:50:08 +0200

    de

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,16 +1,45 @@
-global_start
+section .bss
+    input resb 256
 
 section .data
-msg db "1337" , 10
-len equ $ - msg
-section .text
+    msg db "1337" , 10
+    len equ $ - msg
 
+section .text
+    global _start
+    
+_start:
+    mov rax, 0
+    mov rdi, 0 
+    mov rsi, input
+    mov rdx, 256
+    syscall
+
+    cmp byte [input], 0x34
+    jne _error
+    cmp byte [input +1], 0x32
+    jne _error
+
+_end:
     mov rax, 1
     mov rdi, 1 
     mov rsi, msg
-    mov rdx, len 
+    mov rdx, len
     syscall
 
+
     mov rax, 60
-    mov rdi, 0 
+    mov rdi, 0
     syscall
+
+_error:
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+
+
+
+
+
+    

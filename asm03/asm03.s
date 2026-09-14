8f7d9010b12fdd54d202e53a169c75de460213cf
bensam95 <bensam.dev@gmail.com>
2026-09-14 14:40:56 +0200

    ad

---
+++ asm03/asm03.s
@@ -0,0 +1,34 @@
+section .data
+    msg db "1337", 10
+
+section .text
+    global _start
+
+_start:
+    cmp qword [rsp], 2
+    jne _error
+
+    mov rsi, [rsp + 16]
+
+    cmp byte [rsi], '4'       
+    jne _error
+    cmp byte [rsi + 1], '2'   
+    jne _error
+    cmp byte [rsi + 2], 0     
+    jne _error
+
+_end:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 5
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_error:
+    mov rax, 60
+    mov rdi, 1
+    syscall

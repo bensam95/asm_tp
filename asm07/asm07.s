3cf41cfc430caea4a8cc53fd4be7667069ad9c76
bensam95 <bensam.dev@gmail.com>
2026-09-24 16:28:10 +0200

    up

---
+++ asm07/asm07.s
@@ -0,0 +1,165 @@
+global _start
+
+section .bss
+    buffer resb 32
+
+section .text
+_start:
+    cmp qword [rsp], 3
+    jne arg_err
+
+    mov rdi, [rsp + 16]
+    call parse_int
+    mov r12, rax
+
+    mov rdi, [rsp + 24]
+    call parse_int
+    mov r13, rax
+
+    add r12, r13
+    jo calc_err
+
+    mov rdi, r12
+    call print_int
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+parse_int:
+    xor rax, rax
+    xor r8, r8
+    xor rcx, rcx
+
+    movzx rdx, byte [rdi]
+    cmp dl, '-'
+    je .negatif
+    cmp dl, '+'
+    je .positif
+    jmp .loop
+
+.negatif:
+    mov r8, 1
+    inc rdi
+    jmp .loop
+
+.positif:
+    inc rdi
+
+.loop:
+    movzx r10, byte [rdi]
+    test r10, r10
+    jz .fait
+
+    cmp r10, '0'
+    jb calc_err
+    cmp r10, '9'
+    ja calc_err
+
+    sub r10, '0'
+    inc rcx
+
+    mov r9, 10
+    mul r9
+    jo calc_err
+
+    add rax, r10
+    jc calc_err
+
+    inc rdi
+    jmp .loop
+
+.fait:
+    test rcx, rcx
+    jz calc_err
+
+    test r8, r8
+    jz .position
+
+    mov rdx, 0x8000000000000000
+    cmp rax, rdx
+    ja calc_err
+    je .min_int
+
+    neg rax
+    ret
+
+.min_int:
+    mov rax, rdx
+    ret
+
+.position:
+    mov rdx, 0x7FFFFFFFFFFFFFFF
+    cmp rax, rdx
+    ja calc_err
+    ret
+
+print_int:
+    lea rsi, [buffer + 31]
+    mov byte [rsi], 10
+    mov r8, rsi
+
+    test rdi, rdi
+    jnz .non_zero
+
+    dec rsi
+    mov byte [rsi], '0'
+    jmp .output
+
+.non_zero:
+    xor r9, r9
+    mov rax, rdi
+    test rax, rax
+    jns .convert
+
+    mov r9, 1
+    mov r11, 0x8000000000000000
+    cmp rax, r11
+    jne .do_neg
+
+    dec rsi
+    mov byte [rsi], '8'
+    mov rax, 922337203685477580
+    jmp .convert
+
+.do_neg:
+    neg rax
+
+.convert:
+    mov rcx, 10
+.conv_loop:
+    test rax, rax
+    jz .add_sign
+
+    xor rdx, rdx
+    div rcx
+    add dl, '0'
+    dec rsi
+    mov [rsi], dl
+    jmp .conv_loop
+
+.add_sign:
+    test r9, r9
+    jz .output
+    dec rsi
+    mov byte [rsi], '-'
+
+.output:
+    mov rdx, r8
+    sub rdx, rsi
+    inc rdx
+
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+calc_err:
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+arg_err:
+    mov rax, 60
+    mov rdi, 2
+    syscall
